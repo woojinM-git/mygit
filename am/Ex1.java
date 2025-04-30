00f66aaccec5c0bce18c127e97410e4aaaf08d55
mwj02 <145192679+woojinM-git@users.noreply.github.com>
2025-04-30 17:45:52 +0900

    sis 쌍용교육센터 교육내용 1일차

---
+++ am/Ex1.java
@@ -0,0 +1,9 @@
+package am;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		// 프로그램의 시작 - 자바가상머신(JVM)이 자동으로 호출함
+		System.out.print("자바의 꿈");
+		System.out.print("Hello World!");
+	}
+}
---
+++ pm/Ex9_Oper.java
@@ -0,0 +1,28 @@
+package pm;
+
+import java.util.Scanner;
+
+public class Ex9_Oper {
+
+	public static void main(String[] args) {
+		// 키보드로 부터 정수를 하나 받아 변수 v1에 저장한다.
+		// v1의 값이 20대 라면 true 
+		// 20대가 아니라면 false
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("정수");
+		
+		int v1 = scan.nextInt();
+		
+		boolean res = ((v1 >= 20) && (v1 < 30));
+		
+		System.out.println(res);
+		// 키보드로 입력한 값이
+		// 19 = false
+		// 23 = true
+		// 30 = false
+		
+		scan.close();
+	}
+
+}
